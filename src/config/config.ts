5bf344a4cff80c927a81d4bd37779d39d8710ebe
Alix2044 <Ali.jabarkhail@yahoo.com>
2023-01-31 10:42:02 +0100

    ggg

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-
+import {getAuth,createUserWithEmailAndPassword, connectAuthEmulator } from 'firebase/auth'
 const firebaseConfig = {
   apiKey: "AIzaSyAW_vS0D-nklGSfxfw1Ab9BpEYDEltMGTs",
   authDomain: "test-fa178.firebaseapp.com",
@@ -9,5 +9,18 @@ const firebaseConfig = {
   appId: "1:786951890745:web:28bf92648887107621eae8",
   measurementId: "G-4TZZM7PX1S"
 };
+const app = initializeApp(firebaseConfig)
+const auth = getAuth()
+connectAuthEmulator(auth,"http://localhost:19006")
 
-const app = initializeApp(firebaseConfig);
+const email = "email@gmail.com"
+const password = "123"
+createUserWithEmailAndPassword(auth,email,password).then((userCredential)=>{
+  const user = userCredential.user
+  console.log(user)
+})
+
+
+
+
+export default firebaseConfig
